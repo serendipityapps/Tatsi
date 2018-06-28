e98e13d29449199f7c2c325275b90b8550a1eb1d
serendipityapps <info@serendipityappcompany.com>
2018-06-28 15:48:07 +0100

    fix asset filtering by type

--- Tatsi/Config/TatsiConfig.swift
+++ Tatsi/Config/TatsiConfig.swift
@@ -109,7 +109,7 @@ public struct TatsiConfig {
     
     /// Creates and returns the fetch options to use for fetching assets based on the config. Nil if the fetchOptions are not needed.
     internal func assetFetchOptions() -> PHFetchOptions? {
-        guard self.supportedMediaTypes.contains(.image) && self.supportedMediaTypes.contains(.video) && self.supportedMediaSubTypes == nil else {
+        guard self.supportedMediaSubTypes != nil && (self.supportedMediaTypes.contains(.image) || self.supportedMediaTypes.contains(.video)) else {
             return nil
         }
         let fetchOptions = PHFetchOptions()
